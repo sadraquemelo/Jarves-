0546b51bcb256096e55fbf88e11147599c23b329
Sadraque Melo MKT Business <sadraque2604@gmail.com>
2026-06-08 05:31:17 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+// v2 async function handler(req, res) {
   // Configuração rigorosa de CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
